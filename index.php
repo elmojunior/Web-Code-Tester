bc607c24405b99d98591304460c97cf542b8181b
Elmo Júnior <social@elmojunior.com>
2020-05-13 23:45:39 -0300

    Index.php created

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en-us" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    
+    <title>Web Code Test</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="./codes/css.css">
+  </head>
+  <body>
+  
+    <!-- HTML -->
+    <h1>Web Code Teste</h1>
+    <p>A simple project to test web codes.</p>
+    <br>
+
+    <!-- PHP -->
+    <?php include('./codes/php.php'); ?>
+
+    <!-- JavaScript -->
+    <script src="./codes/javascript.js" charset="utf-8"></script>
+    
+  </body>
+</html>
